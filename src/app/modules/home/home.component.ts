72ab6401dff71584190efc41e73c02e0d3af0fce
Navarro rangel de souza <navarrorangel1@hotmail.com>
2025-02-06 14:27:44 -0300

    Add: adicionado formulário de login

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   standalone: false,
@@ -7,5 +8,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
+  loginCard = true;
+
+  loginForm = this.formBuilder.group({
+    email : ['', Validators.required],
+    password: ['', Validators.required],
+  });
+
+  signUpForm = this.formBuilder.group({
+    name: ['', Validators.required],
+    email: ['', Validators.required],
+    password: ['', Validators.required]
+  })
+
+  constructor(private formBuilder : FormBuilder){}
+
+  onSubmitLoginForm(): void {
+    console.log("DADOS DO LOGIN", this.loginForm.value);
+
+  }
+  onSubmitSignUpFormForm(): void {
+    console.log("DADOS DO form de craiacaço", this.signUpForm.value);
+
+  }
+
 
 }
